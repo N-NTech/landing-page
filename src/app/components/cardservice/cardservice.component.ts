08274ce4f710664a309ecd431427b0057ad5f019
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-10-21 22:20:46 -0300

    create services

---
+++ src/app/components/cardservice/cardservice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-cardservice',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './cardservice.component.html',
+  styleUrl: './cardservice.component.css'
+})
+export class CardserviceComponent {
+
+}
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CardPlanComponent } from '../../components/card-plan/card-plan.component';
 import { AccordionComponent } from '../../components/accordion/accordion.component';
+import { CardserviceComponent } from '../../components/cardservice/cardservice.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,CardPlanComponent,AccordionComponent],
+  imports: [NavbarComponent,CardPlanComponent,AccordionComponent,CardserviceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
