7e891efa66b0417d2a0516eea0726359b4527dbe
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-10-25 22:02:25 -0300

    refactor landing

--- src/app/components/cardservice/cardservice.component.ts
+++ src/app/components/cardservice/cardservice.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-cardservice',
@@ -9,5 +12,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './cardservice.component.css'
 })
 export class CardserviceComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit() {
+    // Verifica si el entorno es el navegador antes de inicializar AOS
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 }
